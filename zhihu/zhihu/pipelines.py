c2dcc34af00ebfa05c61a94ffe4ff9c74aa1b840
zhouyong <zhouyong@weijinzaixian.com>
2018-07-17 17:44:13 +0800

    修改bug

--- zhihu/zhihu/pipelines.py
+++ zhihu/zhihu/pipelines.py
@@ -28,18 +28,12 @@ class ZhihuPipeline(object):
         if item.__class__.__name__ == 'ZhihuQuestionItem':
             sql = 'insert into wordpress.zhihu_question(name,url,keywords,answer_count,' \
                   'flower_count,comment_count,date_created,question_id) values (%s,%s,%s,%s,%s,%s,%s,%s)'
-            print('question')
-            print('question')
-            print('question')
-            # self.cursor.execute(sql, (item['name'], item['url'], item['keywords'], item['answer_count'],
-            #                           item['flower_count'], item['comment_count'], item['date_created'], item['question_id']))
+            self.cursor.execute(sql, (item['name'], item['url'], item['keywords'], item['answer_count'],
+                                      item['flower_count'], item['comment_count'], item['date_created'], item['question_id']))
         else:
             if item['upvote_count'] > self.settings['MIN_UPVOTE_COUNT']:
                 sql = 'insert into wordpress.zhihu_answer(question_id,answer_id,author,ans_url,upvote_count,comment_count,excerpt,content)'\
                       'values (%s,%s,%s,%s,%s,%s,%s,%s)'
-                print(item)
-                print('answer')
-                print('answer')
-                # self.cursor.execute(sql, (item['question_id'],item['answer_id'],item['author'], item['ans_url'], item['upvote_count'],
-                #                           item['comment_count'], item['excerpt'],item['content']))
+                self.cursor.execute(sql, (item['question_id'],item['answer_id'],item['author'], item['ans_url'], item['upvote_count'],
+                                          item['comment_count'], item['excerpt'],item['content']))
         self.connect.commit()
